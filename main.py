2187e4bab693e0bb772cfe0ec9f75a7acf779b28
unknown <kevinconwaylax@gmail.com>
2023-12-07 23:39:52 -0500

    screen setup for game

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-https://github.com/PlainOldKevin/Pong.git
+# Imports
+from turtle import Screen
+
+# Create screen and give characteristics
+screen = Screen()
+screen.setup(800, 600)
+screen.bgcolor("black")
+screen.title("Pong")
+
+
+# Exit on click
+screen.exitonclick()
