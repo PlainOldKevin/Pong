02bdadb0b9cf7e0addf5483b0fa6acad04335407
unknown <kevinconwaylax@gmail.com>
2023-12-14 17:19:51 -0500

    added characteristics to paddle class

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 # Imports
 from turtle import Screen
+from paddle import Paddle
 
 # Create screen and give characteristics
 screen = Screen()
 screen.setup(800, 600)
 screen.bgcolor("black")
 screen.title("Pong")
+screen.tracer(0)
 
+# Initialize Paddle,
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+
+screen.listen()
+screen.onkey(r_paddle.up, "Up")
+screen.onkey(r_paddle.down, "Down")
+
+game_on = True
+
+while game_on:
+    screen.update()
 
 # Exit on click
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -1,3 +1,21 @@
 # Imports
 from turtle import Turtle
 
+class Paddle(Turtle):
+
+    # Init method; setting paddle characteristics on creation
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(5, 1) # Multiplies turtle size by w,l respectively (turtle starts at 20x20, so w x 5 = 100 and l x 1 = 20)
+        self.penup() # Lifts the pen up so no pen lines will be drawn from center (instantiation point)
+        self.goto(position)
+
+    def up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
